1563d144d59846f109462fa003663628e47a5b70
Rob Rix <rob.rix@me.com>
2020-07-10 15:17:41 -0400

    Define wrap using liftI.

--- src/Data/Functor/Interval.hs
+++ src/Data/Functor/Interval.hs
@@ -184,7 +184,7 @@ ranges = liftI enumFromTo
 
 
 wrap :: (Applicative f, Real a) => Interval f a -> f a -> f a
-wrap i x = dimensionwise (\ i x -> getI (((I x + sup i) `mod'` size i) + inf i)) i <*> x
+wrap i x = liftI (\ inf sup x -> ((x + sup) `mod'` (sup - inf)) + inf) i <*> x
 
 
 imap :: (f a -> g b) -> Interval f a -> Interval g b

e6ff2b1506a73d4e31711a015d72fe40f23382fe
Rob Rix <rob.rix@me.com>
2020-07-11 11:17:04 -0400

    Use the precise form of interpolation.

--- src/Data/Functor/Interval.hs
+++ src/Data/Functor/Interval.hs
@@ -260,7 +260,7 @@ fromUnit i x = liftI (\ inf sup x ->  x        * (sup - inf)  + inf) i <*> x
 -- lerp 1 = sup
 -- @
 lerp :: (Applicative f, Num a) => a -> Interval f a -> f a
-lerp t = liftI (\ inf sup -> t * (sup - inf) + inf)
+lerp t = liftI (\ inf sup -> (1 - t) * inf + t * sup)
 
 wrap :: (Applicative f, Real a) => Interval f a -> f a -> f a
 wrap i x = liftI (\ inf sup x -> ((x + sup) `mod'` (sup - inf)) + inf) i <*> x

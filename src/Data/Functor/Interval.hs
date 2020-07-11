b006a52649b09b404c3e515fa10e7ba91259d479
Rob Rix <rob.rix@me.com>
2020-07-11 14:30:53 -0400

    Note that the interval can’t be a point interval.

--- src/Data/Functor/Interval.hs
+++ src/Data/Functor/Interval.hs
@@ -262,7 +262,7 @@ liftEnum = liftI enumFromTo
 
 toUnit, fromUnit :: (Applicative f, Fractional a) => Interval f a -> f a -> f a
 
--- | Linearly transform a point in @f@ from an interval of @f@ to the unit interval.
+-- | Linearly transform a point in @f@ from a non-point interval of @f@ to the unit interval.
 toUnit   i x = liftI (\ inf sup x -> (x - inf) / (sup - inf))        i <*> x
 {-# INLINE toUnit #-}
 

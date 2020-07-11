364a0a010c3d6f05ff87eca58bf6e21521682101
Rob Rix <rob.rix@me.com>
2020-07-11 16:36:57 -0400

    Give properties relating wrap to points on the interval.

--- src/Data/Functor/Interval.hs
+++ src/Data/Functor/Interval.hs
@@ -339,6 +339,16 @@ lerp t = liftI (\ inf sup -> (1 - t) * inf + t * sup)
 --
 -- >>> wrap (-pi...pi) (pi + x)
 -- Identity (-pi + x)
+--
+-- @
+-- wrap i (inf i) = inf i
+-- @
+-- @
+-- wrap i (midpoint i) = midpoint i
+-- @
+-- @
+-- wrap i (sup i) = sup i
+-- @
 wrap :: (Applicative f, Real a) => Interval f a -> f a -> f a
 wrap i x = liftI (\ inf sup x -> ((x + sup) `mod'` (sup - inf)) + inf) i <*> x
 {-# INLINE wrap #-}
